0182ad9c339b4341b87f900b1d6656423a5f01e5
Sumit Gore <123761215+Sumitgore77@users.noreply.github.com>
2025-01-11 17:57:15 +0530

    pat1.java

---
+++ Pattern/pat1.java
@@ -0,0 +1,16 @@
+public class pat1
+{
+   public static void main(String [] x)
+   {
+	
+    	for(int i=0;i<=6;i++)
+	    {
+	      int p=1;
+	       for(int j=0; j<i;j++)
+	       {
+		      System.out.print(p+++" ");
+	       }
+	      System.out.println();
+    	}
+   }
+}
