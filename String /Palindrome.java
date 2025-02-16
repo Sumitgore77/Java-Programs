f68901e3bc48e8e444c00d62c75e4861f24f9e0e
Sumit Gore <123761215+Sumitgore77@users.noreply.github.com>
2025-02-16 08:22:11 +0530

    Update Palindrome.java

--- String /Palindrome.java
+++ String /Palindrome.java
@@ -1,1 +1,15 @@
+import java.util.*;
+public class StringTOChar{
 
+  public static void main(String x[])
+  {
+    String str="MY name is Sumit";
+
+    char ch[]=str.toCharArray();
+
+    for(int i=0; i<ch;i++)
+      {
+          System.out.println(ch[i]);
+      }
+  }
+}
